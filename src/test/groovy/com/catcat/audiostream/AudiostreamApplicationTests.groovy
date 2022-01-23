f2daf2aa6b83cb74efed6a1342c2a444b755655b
Iván Peña <ivan@corebooks.com>
2022-01-22 22:45:56 -0600

    Fixed directory structure

--- src/test/groovy/com/catcat/audiostream/AudiostreamApplicationTests.groovy
+++ src/test/groovy/com/catcat/audiostream/AudiostreamApplicationTests.groovy
@@ -1,10 +1,10 @@
-package com.catcat.audiostream
+package com.catcat.catcat
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class AudiostreamApplicationTests {
+class CatcatApplicationTests {
 
 	@Test
 	void contextLoads() {
