dbc2516958253c8eb5f4210ef9181a6894be14e6
Iván Peña <ivan@corebooks.com>
2022-01-22 22:13:43 -0600

    Added initial files

---
+++ src/main/groovy/com/catcat/audiostream/AudiostreamApplication.groovy
@@ -0,0 +1,13 @@
+package com.catcat.audiostream
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AudiostreamApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(AudiostreamApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/catcat/audiostream/AudiostreamApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.catcat.audiostream
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AudiostreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
